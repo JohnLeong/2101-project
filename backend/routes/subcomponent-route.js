be5f171239162ee5e32f94c988e17d4e3f7418d4
Alexthetechkia <58730404+Alexthetechkia@users.noreply.github.com>
2020-10-24 19:17:13 +0800

    add Subcomponent routes
    1. add subcomponent to a component
    2. edit a subcomponent
    3. add/edit subcomponent marks for group of students
    4. add subcomponent marks for group of students

--- backend/routes/subcomponent-route.js
+++ backend/routes/subcomponent-route.js
@@ -11,6 +11,7 @@ import User from "../models/user.js";
 
 const router = express.Router();
 
+/* ----------------------- for debugging ------------------------- */
 //Get all subcomponents
 router.route("/all").get((req, res) => {
     Subcomponent.find()
@@ -18,19 +19,26 @@ router.route("/all").get((req, res) => {
       .catch((err) => res.status(400).json("Error: " + err));
   });
 
-//Get a subcomponent by id
-router.route("/:id").get((req, res) => {
-    Subcomponent.findById(req.params.id)
-      .then((subcomponent) => res.json(subcomponent))
-      .catch((err) => res.status(400).json("Error: " + err));
-  });
+/* --------------------------------------------------------------- */
 
+//Route: GET ...url.../subcomponent/<subcomponentId>
+//Roles: Lecturer, Student
+//Get a subcomponent by id
+router
+    .route("/:id")
+    .all(authenticateJWT([1, 2]))
+    .get((req, res) => {
+        Subcomponent.findById(req.params.id)
+        .then((subcomponent) => res.json(subcomponent))
+        .catch((err) => res.status(400).json("Error: " + err));
+    });
 
 //Route: POST ...url.../subcomponent/new/<componentId>
 //Roles: Lecturer
 //Add a new subcomponent to a component
 router
   .route("/new/:componentId")
+  .all(authenticateJWT([1]))
   .post(async (req, res) => {
     const name = req.body.name;
     const weightage = req.body.weightage;
@@ -57,13 +65,145 @@ router
       .catch((err) => res.status(400).json("Error: " + err));
   });
 
-  //TODO: deleted subcomponent but conponent still have the record
-  //debugging
-  //Delete a component by id
-  router.route("/:id").delete((req, res) => {
-    Subcomponent.findByIdAndDelete(req.params.id)
-      .then(() => res.json("Subcomponent deleted."))
+//Route: PUT ...url.../subcomponent/<subcomponentId>
+//Roles: Lecturer
+//edit a subcomponent by id
+router
+  .route("/:id")
+  .all(authenticateJWT([1]))
+  .put((req, res) => {
+    Subcomponent.findById(req.params.id)
+      .then((Subcomponent) => {
+        Subcomponent.name = req.body.name ?? Subcomponent.name;
+        Subcomponent.weightage = req.body.weightage ?? Subcomponent.weightage;
+        Subcomponent.totalMarks = req.body.totalMarks ?? Subcomponent.totalMarks;
+
+        Subcomponent
+          .save()
+          .then(() => res.json("Subcomponent updated!"))
+          .catch((err) => res.status(400).json("Error: " + err));
+      })
       .catch((err) => res.status(400).json("Error: " + err));
   });
+
+//Route: put ...url.../subcomponent/new/studentmarks/<subcomponentId>
+//Roles: Lecturer
+//Add subcomponent marks for group of students 
+//Input para: subcomponent id
+//Input json: maps{user(student)objectid: marks}
+router
+  .route("/new/studentmarks/:subcomponentId")
+  .all(authenticateJWT([1]))
+  .put((req, res) => {
+    Subcomponent.findById(req.params.subcomponentId)
+        .then((Subcomponent) => {
+            //student marks from db
+            const storedStudentMarks = Subcomponent.studentMarks;
+            var storedMap = new Map();
+            
+            //add to storedMap
+            if(storedStudentMarks!=null){
+                storedStudentMarks.forEach(function(value,key){
+                    storedMap.set(key, value);
+                })
+            }
+
+            //new student marks map
+            const studentMarks = req.body.studentMarks;            
+            //add to storedMap
+            if(studentMarks!=null){
+                for(var newValue in studentMarks){
+                    storedMap.set(newValue, studentMarks[newValue]);
+                }
+            }
+
+            //update to db
+            Subcomponent.studentMarks = storedMap ?? Subcomponent.studentMarks;
+
+            Subcomponent
+            .save()
+            .then(() => res.json("student Marks added!"))
+            .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("Error: " + err));
+    });
+
+    
+
+//Route: put ...url.../subcomponent/edit/studentmarks/<subcomponentId>
+//Roles: Lecturer
+//add or edit subcomponent marks for group of students 
+//Input para: subcomponent id
+//Input json: maps{user(student)objectid: marks}
+router
+  .route("/edit/studentmarks/:subcomponentId")
+  .all(authenticateJWT([1]))
+  .put((req, res) => {
+    Subcomponent.findById(req.params.subcomponentId)
+        .then((Subcomponent) => {
+            //student marks from db
+            const storedStudentMarks = Subcomponent.studentMarks;
+            var storedMap = new Map();
+            
+            //add to storedMap
+            if(storedStudentMarks!=null){
+                storedStudentMarks.forEach(function(value,key){
+                    storedMap.set(key, value);
+                })
+            }
+            else{
+                res.status(400).json("Invalid request");
+                return;
+            }
+
+            //new student marks map
+            const studentMarks = req.body.studentMarks;            
+            //add to storedMap
+            if(studentMarks!=null){
+                for(var newValue in studentMarks){
+                    var boolFound = false;
+                    //find key to change
+                    storedStudentMarks.forEach(function(value,key){
+                        if(key == newValue) {
+                            console.log("keyIN = " + key);
+                            console.log("old marks = " + value);
+                            console.log("new marks = " + studentMarks[newValue]);
+                            storedMap.set(key, studentMarks[newValue]);
+                            boolFound = true;
+                        }
+                    })
+                    //if not found, all
+                    if(!boolFound){   
+                        console.log("cannot find, insert to map");
+                        storedMap.set(newValue, studentMarks[newValue]);
+                        console.log(newValue + " added");
+                    }
+                }
+            }
+
+            //update to db
+            Subcomponent.studentMarks = storedMap ?? Subcomponent.studentMarks;
+
+            Subcomponent
+            .save()
+            .then(() => res.json("student Marks updated!"))
+            .catch((err) => res.status(400).json("Error: " + err));
+        })
+        .catch((err) => res.status(400).json("Error: " + err));
+    });
+
+
+
+/* ----------------------- for debugging ------------------------- */
+//Delete with Problem: deleted subcomponent but conponent still have the record
+//Delete a subcomponent by id
+router.route("/:id")
+.delete(async(req, res) => {
+    Subcomponent.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Subcomponent deleted."))
+        .catch((err) => res.status(400).json("Error: " + err));
+});
+
+/* --------------------------------------------------------------- */
 
 export default router;
