34f027113affe5a087a054ef374e3a80144167a4
Claudia Chan <43591260+x3Kuro@users.noreply.github.com>
2020-11-26 02:31:52 +0800

    Update frontend template

--- frontend/src/Pages/routes.js
+++ frontend/src/Pages/routes.js
@@ -21,17 +21,6 @@ import Dashboard from "@material-ui/icons/Dashboard";
 import DashboardPage from "./Pages/Dashboard.js";
 import TableList from "./Pages/TableList.js";
 
-//External endpoints (backend)
-export const backendUrl = "http://localhost:5000/";
-export const loginUrl = backendUrl + "user/login";                              //POST
-export const importMarksUrl = backendUrl + "subcomponent/new/importmarks/";
-export const createComponentUrl = backendUrl + "component/new/"                 //POST, component/new/<moduleId>
-export const getComponentUrl = backendUrl + "component/"                        //GET, component/<componentId>
-export const updateComponentUrl = backendUrl + "component/"                     //PUT, component/<componentId>
-
-//Internal (frontend)
-export const modulesUrl = "/modules/";
-
 const dashboardRoutes = [
   {
     path: "/dashboard",
